5fa1c0f766f59d0a194f87b2a67b6c4e3dd2af17
Tyler Swystun <86743463+Awesome012@users.noreply.github.com>
2024-05-01 12:31:29 -0700

    cool

--- task2.py
+++ task2.py
@@ -4,10 +4,38 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
+import math
 
-filename = 'task02a.txt'
+filename = 'task02c.txt'
 file = open(filename,'r')
 data = file.read()
 myList = data.split('\n',)
+total = 0
 
-print(myList)
+def pythagorean():
+    global total
+    numberA = myList[0]
+    numberB = myList[1]
+    numberC = myList[2]
+    testlist = []
+    testlist.append(numberA)
+    testlist.append(numberB)
+    testlist.append(numberC)
+    testlist.sort()
+    numberA = int(testlist[0])
+    numberB = int(testlist[1])
+    numberC = float(testlist[2])
+    check = math.sqrt((numberA ** 2) + (numberB ** 2))
+    if check == numberC:
+        total = total + 1
+    for i in range(4):
+        myList.pop(0)
+
+while True:
+    try:
+        pythagorean()
+    except:
+        print(f"there are {total} right triangles")
+        break
+    else:
+        pythagorean()
--- task3.py
+++ task3.py
@@ -7,3 +7,32 @@ Read the contents of task03.txt into your program and determine the points value
 
 For sample data task03.txt, the largest sum should be 68787
 """
+
+filename = 'task02a.txt'
+file = open(filename,'r')
+data = file.read()
+myList = data.split('\n',)
+finalList = []
+
+def test():
+    checklist = []
+    while True:
+        myList.pop(0)
+        if myList[0] == "":
+            cool = sum(checklist)
+            finalList.append(cool)
+            break
+        else:
+            checklist.append(int(myList[0]))
+            
+            
+
+while True:
+    try:
+        test()
+    except:
+        finalList.sort()
+        print(finalList[-1])
+        break
+    else:
+        test()
